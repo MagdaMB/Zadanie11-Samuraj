019ea9c95dd8f06c4bbe94729428c30b8171a31b
MagdaMB <magda.m.badura@gmail.com>
2019-01-13 12:21:50 +0100

    odliczanie czasu do zdarzenia

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-let time = document.querySelector(".result");
+const endTime = new Date('2019-02-25 15:34:00').getTime();
+const spanD = document.querySelector(".d");
+const spanH = document.querySelector(".h");
+const spanM = document.querySelector(".m");
+const spanS = document.querySelector(".s");
 
-const showTime = () => {
-    const date = new Date();
+setInterval(() => {
+        const nowTime = new Date().getTime();
+        const time = endTime - nowTime;
 
-    let hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
-    let minute = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-    let seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+        const days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
+        let hours = Math.floor(((endTime / (1000 * 60 * 60)) - (nowTime / (1000 * 60 * 60))) % 24);
 
+        hours = hours < 10 ? `0${hours}` : hours;
 
-    time.textContent = `Aktualna godzina to: ${hour}:${minute}:${seconds}`;
+        let minutes = Math.floor(((endTime / (1000 * 60)) - (nowTime / (1000 * 60))) % 60);
 
+        minutes = minutes < 10 ? `0${minutes}` : minutes;
 
-}
+        let seconds = Math.floor(((endTime / (1000)) - (nowTime / (1000))) % 60);
 
-setInterval(showTime, 1000);
+        seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+        spanD.textContent = `${days} dni :`;
+        spanH.textContent = `${hours} godzin :`;
+        spanM.textContent = `${minutes} minut :`;
+        spanS.textContent = `${seconds} sekund :`;
+    },
+    1000)
